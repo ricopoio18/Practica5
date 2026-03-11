668349bde71e3125365cc0b0cb94422657abf4d6
Josué <josue.rico@uabc.edu.mx>
2026-03-11 10:12:28 -0700

    Se importaron las clases del repositorio labpoo20242-practica6

---
+++ src/main/java/Practica5/Main.java
@@ -0,0 +1,15 @@
+package Practica5;
+
+public class Main {
+    public static void main(String[] args) {
+        Square square = new Square();
+        square.changeColor("blue");
+        square.makeVisible();
+        Circle circle = new Circle();
+        circle.changeColor("red");
+        circle.makeVisible();
+        Person person = new Person();
+        person.changeColor("green");
+        person.makeVisible();
+    }
+}
